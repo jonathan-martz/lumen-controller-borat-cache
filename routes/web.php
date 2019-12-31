c9c2eab1fb7d2ee82e56dc14c2022b037de65394
jonathan-martz <github@jmartz.de>
2020-01-01 00:53:30 +0100

    fix route url

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/user/view', [
+Route::post('/borat/cache/clear', [
     'middleware' => ['xss', 'https'],
     'uses' => 'App\Http\Controllers\BoratCacheController@clear'
 ]);
